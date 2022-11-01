c3352872c12f3dfe36c6ba91ab0e008dcbed61ae
betu55 <se.bemenet.semegnew@gmail.com>
2022-11-01 18:39:47 -0400

    added some css to the index.html file

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="text-xl font-bold">
+    <div className="text-xl font-bold container mx-auto w-full h-full">
       <div>Hello this is my new react app.</div>
-      <div className="border">
+      <div className="border h-full">
         <iframe
-          title="3D-Model"
+          title="3D Model D-Mannitol"
+          className="w-full h-full"
           src="https://app.vectary.com/p/2EpMpaVJAEmMqlZH127ydQ"
           frameborder="0"
-          width="100%"
-          height="480"
         ></iframe>
       </div>
     </div>
