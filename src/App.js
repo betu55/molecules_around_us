2cbd84d1025d21dff8d028acad048bc4489d6dbb
betu55 <se.bemenet.semegnew@gmail.com>
2022-11-01 18:09:38 -0400

    added the 3d model and a tailwind css utility toolkit for styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-xl font-bold">
+      <div>Hello this is my new react app.</div>
+      <div className="border">
+        <iframe
+          title="3D-Model"
+          src="https://app.vectary.com/p/2EpMpaVJAEmMqlZH127ydQ"
+          frameborder="0"
+          width="100%"
+          height="480"
+        ></iframe>
+      </div>
     </div>
   );
 }
