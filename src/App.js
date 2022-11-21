513bd837e32e2e7dd5c177375521d16806427300
betu55 <se.bemenet.semegnew@gmail.com>
2022-11-20 23:17:54 -0500

    Updated the landing page and added a background video.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
+import bgVid from './Assets/Videos/bg.mp4'
 
 function App() {
   return (
-    <div className="text-xl font-bold container mx-auto w-full h-full">
-      <div>Hello this is my new react app.</div>
-      <div className="border h-full">
-        <iframe
-          title="3D Model D-Mannitol"
-          className="w-full h-full"
-          src="https://app.vectary.com/p/2EpMpaVJAEmMqlZH127ydQ"
-          frameborder="0"
-        ></iframe>
+    <div className="font-mono">
+      <video
+        className="video invisible lg:visible"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
+        <source src={bgVid} />
+      </video>
+      <div className="content mx-8 md:mx-20 pt-10">
+        <div className="text-4xl text-white text-center md:text-left md:text-5xl">
+          <a href="/" className="font-bold hover:text-red-600">
+            D-Mannitol
+          </a>
+        </div>
+        <div>
+          <p className="my-2 text-white text-lg md:text-xl text-center md:text-left">
+            Welcome to the site!
+          </p>
+        </div>
       </div>
     </div>
   );
