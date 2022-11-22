52420583f56a286d63c1d33d1511e6960438c3bb
betu55 <se.bemenet.semegnew@gmail.com>
2022-11-22 16:54:14 -0500

    added some more content in the home page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@ import bgVid from "../Assets/Videos/bg.mp4";
 
 export function Home() {
   return (
-    <div className="font-mono">
+    <div className="font-mono h-full w-full">
       <video
-        className="video invisible lg:visible"
+        className="video invisible lg:visible h-full w-full"
         autoPlay
         loop
         muted
@@ -12,23 +12,40 @@ export function Home() {
       >
         <source src={bgVid} />
       </video>
-      <div className="content mx-8 md:mx-20 pt-10">
-        <div className="text-4xl text-white text-center md:text-left md:text-5xl">
-          <a href="/" className="font-bold hover:text-red-600">
-            D-Mannitol
-          </a>
+      <div className="h-full w-full lg:grid grid-rows-8">
+        <div className="content mx-8 md:mx-20 pt-10 row-span-6 ">
+          <div className="text-4xl text-slate-300 text-center md:text-left md:text-5xl ">
+            <a
+              href="/"
+              className="font-bold hover:text-red-600 hover:animate-pulse"
+            >
+              D-Mannitol
+            </a>
+          </div>
+          <div>
+            <p className="my-2 text-slate-300 text-lg md:text-xl text-center md:text-left">
+              Welcome to the site!
+            </p>
+          </div>
+          <div className="mt-4 hidden lg:block text-slate-300 md:text-lg text-center md:text-left">
+            This is a simple web based experience of the chemical compound
+            commercialy known as D-Mannitol.
+          </div>
+          <div className="visible lg:invisible text-slate-300 text-left md:text-left md:text-lg my-6">
+            For a better experience it is reccomended that you visit this
+            website from a larger device, preferably a{" "}
+            <p className="inline font-bold">laptop</p> or a{" "}
+            <p className="inline font-bold">desktop </p>
+            computer.
+            <p className="my-6">
+              It is also possible to proceed with curtrent setup.
+            </p>
+          </div>
         </div>
-        <div>
-          <p className="my-2 text-white text-lg md:text-xl text-center md:text-left">
-            Welcome to the site!
+        <div className="content lg:mt-10 md:mx-20 row-span-2">
+          <p className="text-slate-300 md:text-lg text-left md:text-left ">
+            And this is the second row
           </p>
-        </div>
-        <div className="visible lg:invisible text-white text-center text-justify md:text-left text-lg my-8">
-          For a better experience it is reccomended that you visit this website
-          from a larger device, preferably a{" "}
-          <p className="inline font-bold">laptop</p> or a{" "}
-          <p className="inline font-bold">desktop </p>
-          computer.
         </div>
       </div>
     </div>
